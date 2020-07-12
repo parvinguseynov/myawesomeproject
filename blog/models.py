79ae582e02d762296590e3f22c9b1fb02431d8b3
Parvin <pervin.guseynov@gmail.com>
2020-07-12 17:17:22 +0400

    Add home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=300)
+    post_date = models.DateTimeField()
+    post_text = models.TextField()
+    post_image = models.ImageField(upload_to='posts_image/')
+
+    # def __str__(self):
+    #     return self.post_text
+
+
+    # def was_published_recently(self):
+    #     return self.post_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+
+
+
+
--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition, Сюда должны добавить созданные нами приложения, а затем сделать makemigrations
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
